da41be8e90bbcdd0907109636c714256fd6360fd
Noah HENOCH <henoch.noah@gmail.com>
2024-01-04 04:20:15 +0100

    Changements mineurs main.c

--- src/main.c
+++ src/main.c
@@ -2,8 +2,7 @@
 #include <string.h>
 #include "modele/case.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     if (argc == 1) {
         printf("Affichage ncurses\n");
     } else if (argc == 2 && strcmp("-no-gui", argv[1]) == 0) {
