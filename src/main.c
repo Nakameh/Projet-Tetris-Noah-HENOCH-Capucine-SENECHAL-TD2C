41fd4ffb0cc44396ae6317ba7404d58c38fb1ec4
Noah HENOCH <henoch.noah@gmail.com>
2024-01-04 05:19:54 +0100

    Affichage du plateau depuis le main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include "modele/case.h"
+#include "modele/plateau.h"
 
 int main(int argc, char const *argv[]){
     if (argc == 1) {
@@ -9,6 +9,16 @@ int main(int argc, char const *argv[]){
         printf("Affichage SDL\n");
     } else {
         printf("[ERREUR] Nombre de paramètres invalides/Paramètre invalide !\n./tetris [-no-gui]\n");
+    }
+    Plateau * p = initPlateau(20,10);
+    printf("Affichage du tableau :\n");
+    for (int i = 0; i < p->nbLigne; i++) {
+        printf("|");
+        for (int j = 0; j < p->nbColonne; j++) {
+            if (p->tab[i * p->nbColonne + j]->occupe) printf("%c%c", p->tab[i * p->nbColonne + j]->color, p->tab[i * p->nbColonne + j]->color);
+            else printf("  ");
+        }
+        printf("|\n");
     }
     return 0;
 }
