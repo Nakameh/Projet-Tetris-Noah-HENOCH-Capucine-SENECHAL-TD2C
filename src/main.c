a66f44f9b7435f171c09f1129c3b01a80db6c4db
Noah HENOCH <henoch.noah@gmail.com>
2024-01-04 01:57:44 +0100

    Gestion des paramètres du projets (Affichage graphique ou non)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Ceci est un test !\n");
+    if (argc == 1) {
+        printf("Affichage ncurses");
+    } else if (argc == 2 && strcmp("-no-gui", argv[0])) {
+        printf("Affichage SDL");
+    } else {
+        printf("[ERREUR] Nombre de paramètres invalides/Paramètre invalide !\n./tetris [-no-gui]");
+    }
     return 0;
 }
