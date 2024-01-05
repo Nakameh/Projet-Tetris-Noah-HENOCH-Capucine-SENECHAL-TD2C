bcf0868aacca9f96342dfa1d888e7a422cf39293
Noah HENOCH <henoch.noah@gmail.com>
2024-01-05 18:58:48 +0100

    Utilisation de la vue ncurses dans le main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-#include "modele/partie.h"
 #include <time.h>
+#include "vue/vueNcurses.h"
 
-//COULEURS : 32 = VIDE; 1 = JAUNE; 2 = BLEU; 3 = ROUGE; 4 = VERST 5 = ORANGE; 6 = ROSE; 7 = VIOLET
+//COULEURS : 32 = VIDE; 1 = JAUNE; 2 = BLEU; 3 = ROUGE; 4 = VERT 5 = BLANC; 6 = MAGENTA; 7 = CYAN
 
 
 int main(int argc, char const *argv[]){
@@ -45,36 +45,10 @@ int main(int argc, char const *argv[]){
     
     Partie * partie = initPartie(nbLigne, nbColonne);
 
-    Plateau * plat = plateauWithPiece(partie->plateauDeJeu, partie->piece);
+    createWindowNcurses(partie);
+    closeWindowNcurses();
 
-    for (int i = 0; i < plat->nbLigne; i++) {
-        printf("|");
-        for (int j = 0; j < plat->nbColonne; j++) {
-            Case * c = getCase(plat, i, j);
-            if (c->occupe) printf("%d%d", c->color, c->color);
-            else printf("  ");
-        }
-        printf("|\n");
-    }    
-    printf("\n\n");
-    deletePlateau(plat);
 
-    for (int k = 0; k < 25; k++) {
-        prochaineEtape(partie);
-        plat = plateauWithPiece(partie->plateauDeJeu, partie->piece);
-
-        for (int i = 0; i < plat->nbLigne; i++) {
-            printf("|");
-            for (int j = 0; j < plat->nbColonne; j++) {
-                Case * c = getCase(plat, i, j);
-                if (c->occupe) printf("%d%d", c->color, c->color);
-                else printf("  ");
-            }
-            printf("|\n");
-        }    
-        printf("\n\n");
-        deletePlateau(plat);
-    }
     deletePartie(partie);
     return 0;
 }
